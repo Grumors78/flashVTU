04ee9afde5dde1b5ed94a21fda9f6392e478a2a6
Dominator <dominionoweiagbekeme@gmail.com>
2026-06-09 03:51:01 -0700

    version-5

--- QuickTopUpBackend/middleware/corsmiddleware.js
+++ QuickTopUpBackend/middleware/corsmiddleware.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 app.use(cors({
   origin: [
     'https://friendly-smile.up.railway.app ',
-    'http://localhost:3000'  // for local dev
+    'http://localhost:3000' 
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
