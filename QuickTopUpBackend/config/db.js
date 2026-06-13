e78a51c3da50a6eb25a122a9d5fad9aa6ce6920d
Dominator <dominionoweiagbekeme@gmail.com>
2026-06-13 13:45:23 -0700

    version-13

--- QuickTopUpBackend/config/db.js
+++ QuickTopUpBackend/config/db.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
-    process.exit(1); 
+    process.exit(1);
   }
 };
-
+s
 module.exports = connectDB;
